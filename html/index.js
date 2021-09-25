bbbef6257e3d07af1f120c33dc8d9db0ec89f217
Hiroto Chujo <hiroto@chujo.dev>
2021-09-25 10:50:59 +0900

    JSONのエンコード見直し

--- html/index.js
+++ html/index.js
@@ -23,7 +23,7 @@ window.createSession = () => {
         .then(async d => {
             console.log(d)
             await pc.setLocalDescription(d)
-            await postData("/sdp", atob(d.toString()))
+            await postData("/sdp", btoa(JSON.stringify(pc.localDescription)))
         })
         .catch(log)
 
